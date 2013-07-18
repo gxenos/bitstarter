b77d4fb56ee849655e5355e3fdff79b8239776f6
George Xenos <gxenos95@gmail.com>
2013-07-18 07:40:53 +0000

    New grader.js

--- grader.js
+++ grader.js
@@ -3,6 +3,7 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var rest = require('restler');
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -42,10 +43,17 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <html_url>', 'Path to website')
         .parse(process.argv);
+    if (program.file){ 
     var checkJson = checkHtmlFile(program.file, program.checks);
     var outJson = JSON.stringify(checkJson, null, 4);
     console.log(outJson);
+} else {
+    var checkJson = checkHtmlFile(rest.get(program.url), program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+}
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
